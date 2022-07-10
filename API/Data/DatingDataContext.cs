b0f6487b24f52e71267a29c5688566421f9060b9
Vasantha <BuddigaVasantha@gmailcom>
2022-07-10 09:02:41 -0400

    Add project files.

---
+++ API/Data/DatingDataContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DatingDataContext : DbContext
+    {
+        public DatingDataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
